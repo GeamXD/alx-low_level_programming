23d1cda035b3da78db49e7eb402f6e7c95dfecde
Musa Godwin <musa.godwin8112>
2022-06-29 20:10:27 +0100

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char var[] = "with proper grammar, but the outcome is a piece of art,";
+printf("%s", var);
+return (0);
+}
