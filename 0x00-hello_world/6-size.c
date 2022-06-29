fcbffd7fa1f9a3f49e8d118fcb51d3db1ad335b7
Musa Godwin <musa.godwin8112>
2022-06-29 21:24:18 +0100

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ long long int ill;
 float f;
 printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(il));
-printf("Size of a long long int: %lld byte(s)\n", (unsigned long)sizeof(ill));
+printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(il));
+printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof(ill));
 printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
