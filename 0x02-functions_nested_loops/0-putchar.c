861707fb9dd51970ecfb2637c7e62c39792ed7f4
Musa Godwin <musa.godwin8112>
2022-07-05 18:56:29 +0100

    prints putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,14 @@
   */
 int main(void)
 {
-	char i[8] = "_putchar";
-	int j;
-
-	for (j = 0; j < 9; j++)
-	{
-		_putchar(i[j]);
-	}
-	_putchar(10);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
