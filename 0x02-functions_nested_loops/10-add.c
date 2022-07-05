b8c925642082bc2d5f10c0626cdd07e1ca3e022e
Musa Godwin <musa.godwin8112>
2022-07-05 20:25:36 +0100

    summation

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *add - adds two integers
+  *@i: first integer
+  *@j: second integer
+  *
+  *Return: sum
+  */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
