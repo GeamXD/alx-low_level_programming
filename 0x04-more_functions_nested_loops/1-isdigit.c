17354c49cb05a13f06f80ff13bb5065556f5ac87
Musa Godwin <musa.godwin8112>
2022-07-07 15:50:07 +0100

    more loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * _isupper - to check for uppercase
+ * _isdigit - to check for uppercase
  * @c: parameter being checked
  * Return: 1 if uppercase and 0 if lowercase
  */
