c3ff79c529c4b4c28b0423113e577bc217b7a9b6
Musa Godwin <musa.godwin8112@gmail.com>
2022-07-12 23:27:00 +0100

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcpy - copies a string
+ * @dest: destination
+ * @src: source
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+
+}
