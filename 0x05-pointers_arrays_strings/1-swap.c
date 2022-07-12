6aba8fd86a0fc8aa00736bd74bfb2b283de58ff5
Musa Godwin <musa.godwin8112@gmail.com>
2022-07-12 17:59:49 +0100

    swaps integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,15 +2,20 @@
 #include "main.h"
 
 /**
- * swap_int - swaps two integers
- * @a: first integer
- * @b: second integer
+ * swap_int - swaps two integers, and it serves as our declaration
+ * @a: *a makes var a, a pointer as it holds the address of var a
+ * giving us the ability to manipulate whats stored at the address
+ * @b: *b makes var b, a pointer as it holds the address of var a
+ * giving us the ability to manipulate whats stored at the addres
  */
 void swap_int(int *a, int *b)
-{
-	int temp;
+/*simple means a and b are pointers and holds an addess*/
+{/*this address can now be manipulated in the code block*/
+	int temp; /* this serves as temporary*/
+	/*var to hold the dereference of pointer a*/
 
-	temp = *a;
+	temp = *a;/* *a is not a pointer but a derefence due to '*'*/
+	/*a pointer in this case would only have a*/
 	*a = *b;
 	*b = temp;
 }
