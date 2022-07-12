3fb8ce8a7525e4b9d97d066e15f419d61e6d5f2a
Musa Godwin <musa.godwin8112@gmail.com>
2022-07-12 18:11:53 +0100

    finds string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * @_strlen - finds the lenght of a string
+ * @s: parameter being checked
+ * Return: string lenght
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
