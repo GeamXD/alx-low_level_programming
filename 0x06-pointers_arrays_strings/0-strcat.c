963f96e140fe35aa92bf367cc6fa40593a909249
Musa Godwin <musa.godwin8112@gmail.com>
2022-07-14 10:25:55 +0100

    string cat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcat - appends a string to another string
+ * @dest: destination
+ * @src: source
+ * Return: char*
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
