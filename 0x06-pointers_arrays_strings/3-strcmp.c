1b71c92337243c98d5530240be83d5c3aa25d5bb
Musa Godwin <musa.godwin8112@gmail.com>
2022-07-14 10:52:59 +0100

    compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strncmp - compares two string
+ * @s1: string 1
+ * @s2: string 2
+ * Return: char*
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
