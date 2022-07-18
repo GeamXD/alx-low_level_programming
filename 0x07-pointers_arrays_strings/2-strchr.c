da365ef1d88c24ec7859864f50595ac59afab0ae
Musa Godwin <musa.godwin8112@gmail.com>
2022-07-18 17:26:25 +0100

    tasks

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strchr - strchr
+ * @s: parameter
+ * @c: parameter
+ * Return: strchr
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
