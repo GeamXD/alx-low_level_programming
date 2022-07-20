ab1bc0080796d57a4d94e8adc718608b582cbbd0
Musa Godwin <musa.godwin8112@gmail.com>
2022-07-20 21:06:13 +0100

    task

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,18 +1,14 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints in reverse
- * @s: string parameter
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: the string
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-	{
-		_putchar('\n');
 		return;
-	}
-	s++;
-	_print_rev_recursion(s);
-	s--;
+
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
