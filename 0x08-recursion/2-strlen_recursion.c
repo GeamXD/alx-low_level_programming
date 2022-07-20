cde5425c32dd6d40213145a41c7e14a988838f9a
Musa Godwin <musa.godwin8112@gmail.com>
2022-07-20 21:09:28 +0100

    hot fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - prints string lenght
- * @s: string parameter
- * Return: lenght of string
+ * _strlen_recursion - returns the length of a string.
+ * @s: the string
+ *
+ * Return: the length of the string
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\n')
-	{
+	if (*s == '\0')
 		return (0);
-	}
-	s++;
-	return (1 + _strlen_recursion(s++));
+
+	return (_strlen_recursion(++s) + 1);
 }
