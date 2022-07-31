12b50bf9cc6be38ca890f3550ed1128a6bf9278d
Musa Godwin <musa.godwin8112@gmail.com>
2022-07-31 17:02:35 +0100

    hot fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,7 +13,7 @@ void puts_half(char *str)
 
 	if (len % 2 == 0)
 	{
-		for (i = (len / 2); i <= len; i++)
+		for (i = (len / 2); i < len; i++)
 		{
 			_putchar(str[i]);
 		}
@@ -22,8 +22,8 @@ void puts_half(char *str)
 
 	if (len % 2 != 0)
 	{
-		hlen = (len - 1) / 2;
-		for (i = hlen - 1; i <= len; i++)
+		hlen = (len + 1) / 2;
+		for (i = hlen; i <= len; i++)
 		{
 			_putchar(str[i]);
 		}
