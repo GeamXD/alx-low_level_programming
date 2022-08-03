1251b50d5a5477b916aa534e73b71a9549ee9876
Musa Godwin <musa.godwin8112@gmail.com>
2022-08-03 16:04:44 +0100

    int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - searches for integer
+ * @array: parameter array
+ * @size: size of array
+ * @cmp: compares the array
+ * Return: 0 if successful, -1 if no matches found
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || !cmp)
+		return (0);
+
+	if (size <= 0)
+		return (-1);
+
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	return (-1);
+}
