88b7ca2da4c09673873b54bcf0d63b2dfde262d3
Musa Godwin <musa.godwin8112@gmail.com>
2022-08-04 18:37:04 +0100

    done

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+
+/**
+ * print_int - prints int
+ * @ap: parameter for list
+ */
+void print_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+
+
+/**
+ * print_float - prints float
+ * @ap: parameter for list
+ */
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+
+
+/**
+ * print_char - prints string
+ * @ap: parameter for list
+ */
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+
+
+/**
+ * print_string - prints string
+ * @ap: parameter for list
+ */
+void print_string(va_list ap)
+{
+	char *res = va_arg(ap, char *);
+
+	if (res == NULL)
+		printf("(nil)");
+	else
+	printf("%s", res);
+}
+
+
+/**
+ * print_all - prints anything
+ * @format: arguments to print
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+
+	int i = 0, j = 0;
+	char *separator = "";
+
+print_anyStruct printType[] = {
+	{"i", print_int },
+	{"f", print_float},
+	{"s", print_string},
+	{"c", print_char},
+	{NULL, NULL}
+};
+
+	va_start(ap, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (*printType[j].type == format[i])
+			{
+				printf("%s", separator);
+				printType[j].printfunc(ap);
+				separator = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+void print_int(va_list ap);
+void print_char(va_list ap);
+void print_float(va_list ap);
+void print_strinf(va_list ap);
+
+/**
+ * struct print_anyStruct - structure definition
+ * @type: type
+ * @printfunc: function to print;
+ */
+typedef struct print_anyStruct
+{
+	char *type;
+	void (*printfunc)(va_list);
+} print_anyStruct;
 
 #endif
