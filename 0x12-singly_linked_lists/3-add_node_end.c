322e720215157000e3fa52bec43702ed0cd74720
Musa Godwin <musa.godwin8112@gmail.com>
2022-08-19 14:19:50 +0100

    fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,7 +12,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *last = *head;
 	size_t len = 0;
 
-	if (new_node == NULL)
+	if (str == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
